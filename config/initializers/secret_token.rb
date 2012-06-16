23f58ccb3a1c1ad8fafd06f2ae92d9760fdb0971
corix <rico.c.marcelo@gmail.com>
2012-06-15 21:32:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HideNotDeleteTest::Application.config.secret_token = '05c3d9e2c912648f6c4261fd51d60adaab07b9e5e3aafd24d22e13d53d72dc5f8a281bf67bc04bb365b913157cbca69d2e66fb97cc7ac93a8e4c7933ecdbfff8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HideNotDeleteTest::Application.config.session_store :cookie_store, key: '_hide_not_delete_test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HideNotDeleteTest::Application.config.session_store :active_record_store
